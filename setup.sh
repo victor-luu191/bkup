00d193fc00b0490ac8d37aa6aa403c99a3d8b7ee
GFG <gfg@GFGs-MacBook-Pro.local>
2018-05-31 16:52:31 +0800

    add setup.sh

---
+++ setup.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+source $HOME/Library/init/utils.sh
+
+# install brew
+if type_exists 'brew'; then
+	e_success "brew already installed"
+else
+	echo "Installing Homebrew..."
+	/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+# install zsh
+if type_exists 'zsh'; then
+	e_success "zsh already installed"
+else
+	echo "Installing zsh..."
+	brew install zsh
+fi
+
+# if type_exists 'oh-my-zsh'; then
+# 	e_success "oh-my-zsh already installed"
+# else
+# 	echo "Installing oh-my-zsh..."
+# 	sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+# fi
+
+echo "Installing wget..."
+brew install wget
+
+if type_exists 'tree'; then
+	e_success "tree already installed"
+else
+	e_warning "tree not installed!"
+	seek_confirmation "Do you want to install it?"
+	if is_confirmed; then
+		brew install tree
+	fi
+fi
+
+if type_exists 'cask'; then
+	e_success "cask already installed"
+else
+	brew install cask 
+fi
+
+# installation need confirmation
+seek_confirmation "Install pyenv to manage python environments?"
+if is_confirmed; then
+	brew install pyenv
+else
+	e_arrow "Nothing installed"
+fi
+
+seek_confirmation "Install silver searcher, a quick and dirty way to search through any file structure for a key word or phrase? Use common flag --context to print out the lines before and after the key phrase."
+if is_confirmed; then
+	brew install the_silver_searcher
+else
+	e_arrow "Nothing installed"
+fi
